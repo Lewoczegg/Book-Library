1c37f02949d72d5b633f1ed76d28c1f663ff3f3d
Lewoczegg <mateusz.lewocz46@gmail.com>
2023-04-15 12:22:43 +0200

    Add book to library functionality

--- js/index.js
+++ js/index.js
@@ -1,39 +1,65 @@
 const addNewBook = document.querySelector('#add-book');
 const modal = document.querySelector('.modal');
 const closeModalButton = document.querySelector('.modal-close');
+const bookForm = document.querySelector('.modal-content');
+const titleInput = document.querySelector('input[placeholder="Title"]');
+const authorInput = document.querySelector('input[placeholder="Author"]');
+const isReadInput = document.querySelector('#read');
+const bookContainer = document.querySelector('.book-container');
+
+let books = [];
 
 function showModal() {
   modal.style.display = 'flex';
 }
-addNewBook.addEventListener('click', showModal);
 
 function closeModal() {
   modal.style.display = 'none';
 }
+
+function addBookToLibrary(event) {
+  event.preventDefault();
+  const title = titleInput.value;
+  const author = authorInput.value;
+  const isRead = isReadInput.checked;
+  const book = { title, author, isRead };
+  books.push(book);
+  saveToLocalStorage();
+  renderBooks();
+  closeModal();
+  bookForm.reset();
+}
+
+function saveToLocalStorage() {
+  localStorage.setItem('books', JSON.stringify(books));
+}
+
+function retrieveFromLocalStorage() {
+  const storedBooks = JSON.parse(localStorage.getItem('books'));
+  if (storedBooks) {
+    books = storedBooks;
+    renderBooks();
+  }
+}
+
+function renderBooks() {
+  bookContainer.innerHTML = '';
+  books.forEach((book, index) => {
+    const bookElement = document.createElement('div');
+    bookElement.classList.add('book');
+    bookElement.dataset.index = index;
+    bookElement.innerHTML = `
+      <p>Title: <span class="title">${book.title}</span></p>
+      <p>Author: <span class="author">${book.author}</span></p>
+      <button class="read">${book.isRead ? 'Read' : 'Unread'}</button>
+      <button class="remove">Remove</button>
+    `;
+    bookContainer.appendChild(bookElement);
+  });
+}
+
+retrieveFromLocalStorage();
+
+addNewBook.addEventListener('click', showModal);
 closeModalButton.addEventListener('click', closeModal);
-// const addBookModal = document.getElementById('addBookModal');
-// const addBookButton = document.getElementById('addBookButton');
-// const addBookForm = document.getElementById('addBookForm');
-// const addBookTitle = document.getElementById('title');
-// const addBookAuthor = document.getElementById('author');
-// const addBookYear = document.getElementById('year');
-// const addBookClose = document.getElementsByClassName('close')[0];
-// const addBookCancel = document.getElementsByClassName('cancel')[0];
-
-// // Add event listeners for showing and hiding the add book modal
-// addBookButton.addEventListener('click', () => addBookModal.style.display = 'block');
-// addBookClose.addEventListener('click', () => addBookModal.style.display = 'none');
-// addBookCancel.addEventListener('click', () => addBookModal.style.display = 'none');
-// window.addEventListener('click', (event) => {
-//   if (event.target == addBookModal) {
-//     addBookModal.style.display = 'none';
-//   }
-// });
-
-// // Define the addBook function
-// function addBook(title, author, year) {
-//   const book = {
-//     title: title,
-//     author: author,
-//     year: year
-//   };
+bookForm.addEventListener('submit', addBookToLibrary);
